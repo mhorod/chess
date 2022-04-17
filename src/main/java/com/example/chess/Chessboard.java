d97be19c4d99f62a8b841a1c3e53fd109afc7037
MisieqqeisiM <gasosaur@gmail.com>
2022-04-17 14:57:22 +0200

    added rendering of legal moves

--- src/main/java/com/example/chess/Chessboard.java
+++ src/main/java/com/example/chess/Chessboard.java
@@ -1,2 +1,160 @@
-package com.example.chess;public class Chessboard {
+package com.example.chess;
+
+import javafx.geometry.Point2D;
+import javafx.scene.Cursor;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.LongToIntFunction;
+
+
+public class Chessboard extends Pane {
+    public final double fieldSize;
+    Field[][] fields = new Field[8][8];
+    List<Field> legalFields = new ArrayList<>();
+    Field highlightedField;
+
+    private class Piece extends Circle {
+        LogicalPiece piece;
+
+        private Piece(LogicalPiece piece, Color fill, Color stroke) {
+            this.piece = piece;
+            setCursor(Cursor.CLOSED_HAND);
+            Field currentField = fieldAt(piece.x, piece.y);
+            setCenterX(currentField.getCenter().getX());
+            setCenterY(currentField.getCenter().getY());
+            setStroke(stroke);
+            setFill(fill);
+            setStrokeWidth(5);
+
+            putDown();
+
+            setOnMousePressed(e -> pickUp());
+
+            setOnMouseDragged(e -> {
+                setCenterX(e.getX());
+                setCenterY(e.getY());
+                Field current = nearestLegal(e.getX(), e.getY());
+                if(current != null) current.highlight();
+            });
+
+            setOnMouseReleased(e -> {
+                piece.makeMove(highlightedField.x, highlightedField.y);
+                setCenterX(fields[piece.x][piece.y].getCenter().getX());
+                setCenterY(fields[piece.x][piece.y].getCenter().getY());
+                putDown();
+                dehighlightAll();
+            });
+        }
+
+        void pickUp() {
+            toFront();
+            Field current = fields[piece.x][piece.y];
+            current.highlight();
+            setLegalFields(piece.getLegalMoves());
+            setRadius(fieldSize*0.5);
+        }
+
+        void putDown() {
+            setRadius(fieldSize*0.4);
+            setLegalFields(List.of());
+        }
+    }
+
+    private class Field extends Rectangle {
+        int x, y;
+        boolean dark;
+        private Field(boolean dark, int x, int y) {
+            super(fieldSize, fieldSize, fieldSize, fieldSize);
+            this.dark = dark;
+            this.x = x;
+            this.y = y;
+            toNormal();
+            setStrokeType(StrokeType.INSIDE);
+            setStroke(Color.color(1, 0, 1));
+            setStrokeWidth(0);
+        }
+
+        Point2D getCenter() {
+            return new Point2D(x * fieldSize + fieldSize / 2, y * fieldSize + fieldSize / 2);
+        }
+
+        void highlight() {
+            dehighlightAll();
+            highlightedField = this;
+            setStrokeWidth(4);
+        }
+
+        void dehighlight() {
+            setStrokeWidth(0);
+        }
+
+        void markAsLegal() {
+            legalFields.add(this);
+            setFill(dark ? Color.color(0.2, 0, 0.2) : Color.color(0.8, 0.3, 0.8));
+        }
+
+        void toNormal() {
+            setFill(dark ? Color.color(0.1, 0.1, 0.1) : Color.color(0.7, 0.7, 0.7));
+        }
+    }
+
+    Field fieldAt(double realX, double realY) {
+        int x = (int) Math.floor(realX/fieldSize);
+        int y = (int) Math.floor(realY/fieldSize);
+        if(x >= 0 && x < 8 && y >= 0 && y < 8) return fields[x][y];
+        else return null;
+    }
+
+    Field nearestLegal(double realX, double realY) {
+        return legalFields.stream().min((a, b) -> (int) Math.signum(distance2(a, realX, realY) - distance2(b, realX, realY))).orElse(null);
+    }
+
+    static double distance2(Field a, double x, double y) {
+        double deltaX = a.getCenter().getX() - x;
+        double deltaY = a.getCenter().getY() - y;
+        return deltaX * deltaX + deltaY * deltaY;
+    }
+
+    void dehighlightAll() {
+        if(highlightedField != null) highlightedField.dehighlight();
+        highlightedField = null;
+    }
+
+    void setLegalFields(List<LogicalField> fields) {
+        for(Field f : legalFields)
+            f.toNormal();
+        legalFields.clear();
+        for(LogicalField f : fields)
+            this.fields[f.x()][f.y()].markAsLegal();
+    }
+
+
+    public Chessboard(double fieldSize) {
+        this.fieldSize = fieldSize;
+        setMaxWidth(8*fieldSize);
+        setMaxHeight(8*fieldSize);
+        GridPane grid = new GridPane();
+
+
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                fields[x][y] = new Field((x + y) % 2 == 1, x, y);
+                grid.add(fields[x][y], x, y);
+            }
+        }
+
+        getChildren().add(grid);
+
+        for(int x = 0; x < 14; x++) {
+            Piece circle = new Piece(new LogicalPiece(0, 0), Color.color(1, .5, 1), Color.color(1, 0, 1));
+            getChildren().add(circle);
+        }
+    }
 }
--- src/main/java/com/example/chess/Main.java
+++ src/main/java/com/example/chess/Main.java
@@ -19,60 +19,7 @@ import java.util.Stack;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-        Pane board = new Pane();
-        GridPane chessboard = new GridPane();
-        double s = 60; // side of rectangle
-        board.setMaxWidth(8*s);
-        board.setMaxHeight(8*s);
-        Rectangle[][] fields = new Rectangle[8][8];
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                Rectangle r = new Rectangle(s, s, s, s);
-                r.setPickOnBounds(true);
-                if ((i+j) % 2 == 0) r.setFill(Color.color(0.7, 0.7, 0.7));
-                else r.setFill(Color.color(0.1, 0.1, 0.1));
-                r.setStrokeType(StrokeType.INSIDE);
-                r.setStroke(Color.color(1.0, 0.0, 1.0));
-                r.setStrokeWidth(0);
-                fields[j][i] = r;
-                chessboard.add(r, j, i);
-            }
-        }
-
-        Circle circle = createCircle("#ff00ff", "#ff88ff",100);
-        circle.setCursor(Cursor.CLOSED_HAND);
-        circle.setRadius(s*0.4);
-        circle.setCenterX(s/2);
-        circle.setCenterY(s/2);
-        circle.setOnMousePressed(e -> {
-            circle.setRadius(s*0.5);
-        });
-
-
-        circle.setOnMouseDragged(e -> {
-            circle.setCenterX(e.getX());
-            circle.setCenterY(e.getY());
-            int x = (int) Math.floor(e.getX()/s);
-            int y = (int) Math.floor(e.getY()/s);
-            if(x >= 0 && x < 8 && y >= 0 && y < 8) {
-                for(int i = 0; i < 8; i++)
-                    for(int j = 0; j < 8; j++)
-                        fields[i][j].setStrokeWidth(0);
-                fields[x][y].setStrokeWidth(4);
-            }
-        });
-
-        circle.setOnMouseReleased(e -> {
-            circle.setRadius(s*0.4);
-            circle.setCenterX(Math.floor(e.getX() / s) * s + s/2);
-            circle.setCenterY(Math.floor(e.getY() / s) * s + s/2);
-            for(int i = 0; i < 8; i++)
-                for(int j = 0; j < 8; j++)
-                    fields[i][j].setStrokeWidth(0);
-        });
-
-        board.getChildren().add(chessboard);
-        board.getChildren().add(circle);
+        Pane board = new Chessboard(60);
         VBox pane = new VBox();
         pane.setAlignment(Pos.CENTER);
         pane.getChildren().add(board);
@@ -80,20 +27,8 @@ public class Main extends Application {
         Scene scene = new Scene(pane, 1024, 800, true);
         scene.setFill(Color.color(0.2,0.2,0.2));
         primaryStage.setScene(scene);
-        primaryStage.setTitle("2D Example");
-
+        primaryStage.setTitle("Epic chess");
         primaryStage.show();
-    }
-
-    private Circle createCircle(String strokeColor, String fillColor, double x) {
-        Circle circle = new Circle();
-        circle.setCenterX(x);
-        circle.setCenterY(200);
-        circle.setRadius(50);
-        circle.setStroke(Color.valueOf(strokeColor));
-        circle.setStrokeWidth(5);
-        circle.setFill(Color.valueOf(fillColor));
-        return circle;
     }
 
     public static void main(String[] args) {
