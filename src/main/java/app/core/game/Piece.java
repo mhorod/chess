021b115a36ea5042a7f66a968b855c90f8c9aa20
Michal Horodecki <michalhorodecki2002@gmail.com>
2022-04-22 19:50:09 +0200

    created dummy architecture with todos

---
+++ src/main/java/app/core/game/Piece.java
@@ -0,0 +1,8 @@
+package app.core.game;
+
+/**
+ * Abstract piece for any game
+ * TODO: fill in methods that are common for all games
+ */
+public interface Piece {
+}
---
+++ src/main/java/app/core/interactor/Player.java
@@ -0,0 +1,9 @@
+package app.core.interactor;
+
+import app.core.game.*;
+
+/**
+ * Player exposed to external controllers such as UI, abstracts out player number and input method
+ */
+public abstract class Player<M extends Move<P>, P extends Piece> {
+}
