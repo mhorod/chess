e26e550ba41bd6c4fbb46a09f028cd5f836e3d4e
Michal Horodecki <michalhorodecki2002@gmail.com>
2022-04-24 13:21:20 +0200

    Applied sugested changes

--- src/main/java/app/core/game/Move.java
+++ src/main/java/app/core/game/Move.java
@@ -4,5 +4,5 @@ package app.core.game;
  * Abstract move for all games
  * TODO: Fill fields, add subclasses, possibly make class abstract
  */
-public class Move<P extends Piece> {
+public interface Move<P extends Piece> {
 }
