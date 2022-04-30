c96e2b4ff39df43669fd4e2961796e051347b23e
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2022-04-30 18:31:48 +0200

    Added ChessMove interface

---
+++ src/main/java/app/chess/ChessMove.java
@@ -0,0 +1,10 @@
+package app.chess;
+
+import app.core.game.moves.*;
+
+/**
+ * Yes, this interface does absolutely nothing.
+ * @author dominik
+ */
+public interface ChessMove extends Move<ChessPiece> {
+}
