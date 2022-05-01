bdc031450acd3017423b778d665b10cb732a3959
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2022-05-01 14:09:08 +0200

    Pawns now can move

--- src/main/java/app/chess/pieces/Pawn.java
+++ src/main/java/app/chess/pieces/Pawn.java
@@ -1,5 +1,6 @@
 package app.chess.pieces;
 
+import app.chess.*;
 import app.chess.moves.*;
 import app.core.game.*;
 
@@ -18,6 +19,30 @@ public class Pawn extends ChessPiece {
 
     @Override
     public List<ChessMove> getPotentialMoves() {
-        return Collections.emptyList();
+        int player = this.getPlayer();
+
+        int twoMovesForwardRank = player == 0 ? 2 : 7;
+        int multiplier = player == 0 ? 1 : -1;
+
+        ArrayList<ChessMove> potentialMoves = new ArrayList<>();
+
+        //Pawn has maximally 4 possible moves: 2 forward, 1 to left, 1 to right
+
+        int currentRank = this.getPosition().rank();
+        int currentFile = this.getPosition().file();
+
+        if(currentRank == twoMovesForwardRank){
+            Field whereToGo = new Field(currentRank + 2 * multiplier, currentFile);
+            potentialMoves.add(new NormalMove(this, whereToGo)); //no Field validation is needed in this case (because if you go 2 forward then, you are on a rank that guarantees that you won't get out of range)
+        }
+
+        for(int fileModifier = -1; fileModifier <= 1; fileModifier++){
+            //Moves that go 1 forward
+            Field whereToGo = new Field(currentRank + multiplier, currentFile + fileModifier);
+            if(Chess.fieldIsValid(whereToGo)){
+                potentialMoves.add(new NormalMove(this,whereToGo));
+            }
+        }
+        return potentialMoves;
     }
 }
