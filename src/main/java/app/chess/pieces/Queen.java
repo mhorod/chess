128245c23575e84794c6f09eb8d6e277e15e5a13
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2022-05-02 11:10:58 +0200

    Queen can now move

--- src/main/java/app/chess/pieces/Queen.java
+++ src/main/java/app/chess/pieces/Queen.java
@@ -1,5 +1,6 @@
 package app.chess.pieces;
 
+import app.chess.*;
 import app.chess.moves.*;
 import app.core.game.*;
 
@@ -18,6 +19,25 @@ public class Queen extends ChessPiece {
 
     @Override
     public List<ChessMove> getPotentialMoves() {
-        return Collections.emptyList();
+        //Queen can move like a Rook and Bishop, somewhat standing on the same place
+        //This means that there is no sense in writing this piece's logic, we might just create a fake rook and fake bishop with the same Field and then basically merge their results
+
+        ChessPiece fakeRook = new Rook(this.getPosition(),false); //We don' care about any other parameter than the position, because pieces don't care about interactions with others when returning potential moves
+        ChessPiece fakeBishop = new Bishop(this.getPosition(), false);
+
+        List<ChessMove> fakePotentialMoves = fakeRook.getPotentialMoves();
+        fakePotentialMoves.addAll(fakeBishop.getPotentialMoves());
+
+        //We cannot return above list right away, because it contains references to fake pieces
+        //Thus, we will create a new one with correct reference to our piece
+
+        ArrayList<ChessMove> potentialMoves = new ArrayList<>();
+
+        for(ChessMove fakePotentialMove : fakePotentialMoves){
+            ChessMove legitimatePotentialMove = new NormalMove(this, fakePotentialMove.getField());
+            potentialMoves.add(legitimatePotentialMove);
+        }
+        
+        return potentialMoves;
     }
 }
