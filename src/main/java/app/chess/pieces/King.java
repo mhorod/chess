25e8022251d5dfe26d140b50c8a80826aa961e54
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2022-05-02 12:18:26 +0200

    Kings can now move as well

--- src/main/java/app/chess/pieces/King.java
+++ src/main/java/app/chess/pieces/King.java
@@ -1,5 +1,6 @@
 package app.chess.pieces;
 
+import app.chess.*;
 import app.chess.moves.*;
 import app.core.game.*;
 
@@ -18,6 +19,23 @@ public class King extends ChessPiece {
 
     @Override
     public List<ChessMove> getPotentialMoves() {
-        return Collections.emptyList();
+        ArrayList<ChessMove> potentialMoves = new ArrayList<>();
+
+        int currentRank = this.getPosition().rank();
+        int currentFile = this.getPosition().file();
+
+        //We will need two loops that account for every possible move king can make (except for the one where that piece stays in its place)
+        for(int rankModifier = -1; rankModifier <= 1; rankModifier++){
+            for(int fileModifier = -1; fileModifier <= 1; fileModifier++){
+                if(rankModifier != 0 || fileModifier != 0){
+                    //Because move that doesn't move is not the greatest move, I'd say
+                    Field potentialField = new Field(currentRank + rankModifier, currentFile + fileModifier);
+                    if(Chess.fieldIsValid(potentialField)){
+                        potentialMoves.add(new NormalMove(this,potentialField));
+                    }
+                }
+            }
+        }
+        return potentialMoves;
     }
 }
