ef3b368bb1d80478eafbee2f3a0916ab91b36700
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2022-05-14 15:18:03 +0200

    Extracted functions responsible for making moves to another class

--- src/main/java/app/chess/Chess.java
+++ src/main/java/app/chess/Chess.java
@@ -1,7 +1,6 @@
 package app.chess;
 
-import app.chess.moves.Castle;
-import app.chess.moves.ChessMove;
+import app.chess.moves.*;
 import app.chess.pieces.ChessPieceKind;
 import app.chess.rules.*;
 import app.chess.utils.*;
@@ -17,7 +16,7 @@ public class Chess implements Game<ChessMove, ChessPiece> {
     ChessPiece[][] board;
 
     private final StateManager manager = new StateManager();
-
+    private final Mover mover = new StandardMover();
     private final Validator validator = new StandardValidator();
 
     public Chess(Board board) {
@@ -68,151 +67,9 @@ public class Chess implements Game<ChessMove, ChessPiece> {
         return validator.getLegalMoves(piece,board);
     }
 
-    private void resetWasMoved() {
-        var allPieces = getAllPieces();
-
-        for (var currentPiece : allPieces) {
-            currentPiece.unwrap().resetMoved();
-        }
-    }
-
-    /**
-     * Executes castling under assumption that function calling it made sure that such castling is, indeed, legal
-     */
-    private List<ChessPiece> castleMove(Castle move) {
-        Field whereRookIs = Utils.getRookPositionBasedOnCastling(move, board);
-
-        int rookRank = whereRookIs.rank();
-        int rookFile = whereRookIs.file();
-
-        //Castling can't kill anything, etc.
-        //Just changing positions of 2 pieces, nothing can go wrong
-        //Right?
-
-        var king = move.getPiece();
-        int kingRank = king.getPosition().rank(); //Yes, kingRank will be the same as rookRank
-        //In fact, let's just have an additional assertion here
-
-        if (kingRank != rookRank) {
-            throw new BoardDiscrepancy();
-        }
-
-        int kingFile = king.getPosition().file();
-
-        var rook = board[rookRank][rookFile];
-
-        int newRookFile = rookFile == 1 ? 4 : 6; //It is just this way
-        //Rank will stay the same, obviously
-
-        board[rookRank][rookFile] = null;
-        board[kingRank][kingFile] = null;
-
-        board[move.getField().rank()][move.getField().file()] = king;
-        king.unwrap().move(move.getField());
-
-        board[rookRank][newRookFile] = rook;
-        rook.unwrap().move(new Field(rookRank, newRookFile));
-
-        return List.of(move.getPiece(), rook);
-    }
-
     @Override
     public List<ChessPiece> makeMove(int player, ChessMove move) {
-
-        if (move.getPiece().getPlayer() != player) {
-            throw new PlayerDiscrepancy();
-        }
-
-
-        //It's better to be sure that the move we get is actually legal
-        List<ChessMove> acceptableMoves = this.getLegalMoves(player, move.getPiece());
-        if (!acceptableMoves.contains(move)) {
-            System.err.println(acceptableMoves);
-            System.err.println(move);
-            throw new IllegalMove();
-        }
-
-
-        ///TODO: MAKE IT NOT FAIL WITH PENDING PROMOTION
-        manager.switchCurrentPlayer();
-
-
-        //Now, if castling is involved, I'm going to delegate it to another function because otherwise this will be too messy
-        if (move.getClass() == Castle.class) {
-            //I'm sure checking it that way won't cause any issues down the line
-            return castleMove((Castle) move);
-        }
-
-        if (manager.thereIsPromotionPending()) {
-            ///TODO: IMPLEMENT PROMOTION
-            manager.markPromotionAsDone();
-        }
-
-        resetWasMoved(); //Because things will be overwritten
-
-        //Given the fact that this move is legal, we can simply execute it now
-        if (move.getPiece().getKind() == ChessPieceKind.PAWN && (move.getField().rank() == SIZE || move.getField()
-                                                                                                       .rank() == 1)) {
-            //That's funny because it will result in a pawn promotion
-            if (manager.thereIsPromotionPending()) {
-                throw new RuntimeException("This shouldn't even be mathematically possible");
-            }
-            ///TODO: IMPLEMENT PROMOTION
-            //pendingPromotion = true;
-        }
-
-        int newRank = move.getField().rank();
-        int newFile = move.getField().file();
-
-        int oldRank = move.getPiece().getPosition().rank();
-        int oldFile = move.getPiece().getPosition().file();
-
-        var wasHereBefore = board[newRank][newFile];
-
-        if (wasHereBefore != null && wasHereBefore.getPlayer() == player) {
-            throw new IllegalMove(); //This shouldn't even be possible because we've validated that already, but just to be sure
-        }
-
-        if (wasHereBefore != null) {
-            wasHereBefore.unwrap().kill();
-            board[newRank][newFile] = move.getPiece();
-        } else {
-            board[newRank][newFile] = move.getPiece();
-
-            //you'd think that we are done
-            //and you'd be wrong
-            //because en passant exists
-            //to mess with our code
-
-            if (move.getPiece().getKind() == ChessPieceKind.PAWN && oldFile - newFile != 0) {
-                //So, there is nothing in there but we are going sideways
-                //This means en passant
-                //So we'll just remove whatever stays in enpassantable place
-                wasHereBefore = board[oldRank][newFile];
-                if (wasHereBefore == null) {
-                    //?????????
-                    throw new IllegalMove();
-                } else {
-                    wasHereBefore.unwrap().kill();
-                    board[oldRank][newFile] = null;
-                }
-            }
-
-        }
-
-
-        board[oldRank][oldFile] = null;
-
-        move.getPiece().unwrap().move(move.getField());
-
-        //After executing the move, we can finally return a list informing what's happening on the board
-        ArrayList<ChessPiece> changedList = new ArrayList<>();
-        changedList.add(move.getPiece());
-        if (wasHereBefore != null) {
-            changedList.add(wasHereBefore);
-        }
-
-        return changedList;
+        return mover.makeMove(player, move, board, manager);
     }
 
     @Override
