0da6623569d10d585ff29198fce3dd06ace2d85e
Michal Horodecki <michalhorodecki2002@gmail.com>
2022-06-04 20:06:07 +0200

    moved images to separate directorie, renamed bishopHQ to elephant

--- src/main/java/app/ui/games/chess/ChessImages.java
+++ src/main/java/app/ui/games/chess/ChessImages.java
@@ -11,12 +11,12 @@ import static app.chess.pieces.ChessPieceKind.*;
 public class ChessImages {
 
     private static final Map<ChessPieceKind, Image> pieces = Map.ofEntries(
-            Map.entry(KNIGHT, new Image(Images.class.getResource("/img/knight.png").toString())),
-            Map.entry(KING, new Image(Images.class.getResource("/img/king.png").toString())),
-            Map.entry(ROOK, new Image(Images.class.getResource("/img/rook.png").toString())),
-            Map.entry(BISHOP, new Image(Images.class.getResource("/img/bishop.png").toString())),
-            Map.entry(QUEEN, new Image(Images.class.getResource("/img/queen.png").toString())),
-            Map.entry(PAWN, new Image(Images.class.getResource("/img/pawn.png").toString())));
+            Map.entry(KNIGHT, new Image(Images.class.getResource("/img/chess/knight.png").toString())),
+            Map.entry(KING, new Image(Images.class.getResource("/img/chess/king.png").toString())),
+            Map.entry(ROOK, new Image(Images.class.getResource("/img/chess/rook.png").toString())),
+            Map.entry(BISHOP, new Image(Images.class.getResource("/img/chess/bishop.png").toString())),
+            Map.entry(QUEEN, new Image(Images.class.getResource("/img/chess/queen.png").toString())),
+            Map.entry(PAWN, new Image(Images.class.getResource("/img/chess/pawn.png").toString())));
 
     public static Image getPieceImage(ChessPieceKind pieceType) {
         return pieces.get(pieceType);
--- src/main/java/app/ui/utils/Images.java
+++ src/main/java/app/ui/utils/Images.java
@@ -6,8 +6,8 @@ import javafx.scene.image.Image;
  * Images used to render menus
  */
 public class Images {
-    public static final Image logo = new Image(Images.class.getResource("/img/logo.png").toString());
-    public static final Image button = new Image(Images.class.getResource("/img/button.png").toString());
-    public static final Image longButton = new Image(Images.class.getResource("/img/long_button.png").toString());
-    public static final Image elephant = new Image(Images.class.getResource("/img/bishopHQ.png").toString());
+    public static final Image logo = new Image(Images.class.getResource("/img/app/logo.png").toString());
+    public static final Image button = new Image(Images.class.getResource("/img/app/button.png").toString());
+    public static final Image longButton = new Image(Images.class.getResource("/img/app/long_button.png").toString());
+    public static final Image elephant = new Image(Images.class.getResource("/img/app/elephant.png").toString());
 }
